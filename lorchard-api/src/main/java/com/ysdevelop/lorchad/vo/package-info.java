f25ff3706d52e9f7c1cb08136049055e0e26d34c
huang-menghang <449043209@qq.com>
2018-08-21 11:03:25 +0800

    初始化提交dev分支；

---
+++ lorchard-api/src/main/java/com/ysdevelop/lorchad/vo/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author oldHuang
+ *
+ * @Package com.ysdevelop.lorchad.vo
+ * 
+ * @Description TODO
+ *
+ * @Date 2018年8月21日
+ *
+ * @Version
+ *
+ */
+package com.ysdevelop.lorchad.vo;
