d45c770435102393f1c0c408d3a37a82ac850ebc
黑大帅 <1325028794@qq.com>
2018-09-06 15:35:54 +0800

    添加部分店铺装修

---
+++ lorchard-merchant/src/main/java/com/ysdevelop/lorchard/merchant/controller/ShopDecorationController.java
@@ -0,0 +1,18 @@
+package com.ysdevelop.lorchard.merchant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/shopDecoration")
+public class ShopDecorationController {
+
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String index() {
+
+		return "decoration/index";
+	}
+	
+}
