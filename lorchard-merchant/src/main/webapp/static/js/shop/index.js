4d2700978286bacd3417472ba994e989d3188dfc
zhangzesen <3151059861@qq.com>
2018-09-19 11:38:52 +0800

    websocket移到店铺的js

--- lorchard-merchant/src/main/webapp/static/js/shop/index.js
+++ lorchard-merchant/src/main/webapp/static/js/shop/index.js
@@ -6,6 +6,87 @@ var shop_index_ops = {
 	},
 	// 初始化组件
 	inintComponent : function() {
+		var websocket_ops = {
+				init:function(){
+					this.initMessageWebscoket();
+					this.eventBind();
+				},
+				initMessageWebscoket : function() {
+					var websocket;
+					console.log("初始化websocket");
+			// 首先判断是否 支持 WebSocket
+			if ('WebSocket' in window) {
+				websocket = new WebSocket("ws://192.168.1.102:9000/");
+			} else if ('MozWebSocket' in window) {
+				websocket = new MozWebSocket("ws://192.168.1.102:9000"
+					+ "/websocket");
+			} else {
+				websocket = new SockJS("http://" + "192.168.1.102:9000"
+					+ "/socketjs");
+			}
+
+			// 打开时
+			websocket.onopen = function(evnt) {
+				// 发送上线消息
+				var onLineMessage = {};
+				onLineMessage.fromMerchantId = 1;
+				onLineMessage.messageConent = "商家客户端id1,上线";
+				onLineMessage.messageType = "ON_LINE";
+				var jsonOnLineMessage= JSON.stringify(onLineMessage);
+				console.log("json message --->"+jsonOnLineMessage);
+				websocket.send(jsonOnLineMessage);
+			};
+			// 处理消息时
+			websocket.onmessage = function(evnt) {
+				var message = JSON.parse(event.data);
+				console.log(message);
+				var messageType = message.messageType;
+				switch(messageType){
+					case "MERCHANT":
+					$.each(message.messageConent,function(i,v){
+						layer.alert(v.conent);
+					});
+					break;
+					default:
+					break;
+				}
+
+			};
+			window.onbeforeunload = function() {
+				websocket.close();
+			}
+			function closeWebSocket() {
+				websocket.close();
+			}
+
+			// 发送消息
+			function send() {
+				var message = "@heart";
+				heartCheck.start();
+			}
+			var heartCheck = {
+				timeout : 5000,
+				timeoutObj : null,
+				reset : function() {
+					clearTimeout(this.timeoutObj);
+					this.start();
+				},
+				start : function() {
+					this.timeoutObj = setTimeout(function() {
+						websocket.send("@heart");
+					}, this.timeout)
+				}
+			}
+
+		},
+		eventBind:function(){
+
+		}
+		}	
+		$(function(){
+		websocket_ops.init();
+		})
+		
 		// 初始化流量统计图
 		$.ajax({
 			url : WEB_ROOT + '/shop/dailyShopFlow',
@@ -56,3 +137,5 @@ var shop_index_ops = {
 $(function() {
 	shop_index_ops.init();
 });
+
+
